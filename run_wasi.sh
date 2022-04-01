724c9bf9810650cf0f62af2b6f4c4a69fefc0cf9
Harald Hoyer <harald@profian.com>
2022-04-01 10:49:25 +0200

    fix: s/LISTEN_FDS/FD_COUNT/g
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- run_wasi.sh
+++ run_wasi.sh
@@ -1,2 +1,2 @@
 cargo +nightly build --target wasm32-wasi --example hello_wasi
-wasmtime run --tcplisten 127.0.0.1:3000 --env 'LISTEN_FDS=1' target/wasm32-wasi/debug/examples/hello_wasi.wasm
+wasmtime run --tcplisten 127.0.0.1:3000 --env 'FD_COUNT=1' target/wasm32-wasi/debug/examples/hello_wasi.wasm
